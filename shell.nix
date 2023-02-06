59f94717b5a9ba6d5e67e6d9988d77caf1aae35b
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2023-02-06 18:06:05 +0400

    Don't use workspace for this library
    * Move switchboardd and switchboard-cli out of the workspace
    * Move switchboard-gui out of the workspace

--- shell.nix
+++ shell.nix
@@ -1,25 +1,10 @@
-let
-  pkgs = import <nixpkgs> {  };
-in
-  with pkgs;
-  mkShell {
-    buildInputs = [
-      pkgconfig
-      openssl
-      sass
-      glib
-      cairo
-      pango
-      atk
-      gdk-pixbuf
-      libsoup
-      gtk3
-      webkitgtk
-      librsvg
-      patchelf
-      trunk
-      dbus
-      nodePackages_latest.pnpm
-    ];
-    WEBKIT_DISABLE_COMPOSITING_MODE = 1;
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    pkgconfig
+    openssl
+  ];
 }
--- switchboard-gui/src/lib/Amount.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
- // amount in Satoshi
- export let amount: number;
-</script>
-<span>
-    {amount / 1_0000_0000} BTC
-</span>
