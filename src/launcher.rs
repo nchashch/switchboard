026541f2ce1cec0369ec98d25c9d0405b52ab675
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2023-02-21 20:53:47 +0400

    Simplify codebase
    * Get rid of all async await calls
    * Don't use tokio
    * Don't use jsonrpsee
    * Don't start a server
    * Don't define an API

--- src/launcher.rs
+++ src/launcher.rs
@@ -1,4 +1,4 @@
-use crate::api::SidechainClient;
+use crate::client::SidechainClient;
 use crate::config::Config;
 use anyhow::Result;
 use bytes::Buf;
@@ -7,17 +7,77 @@ use std::path::Path;
 use tar::Archive;
 
 pub struct Daemons {
-    pub main: tokio::process::Child,
-    pub zcash: tokio::process::Child,
-    pub ethereum: tokio::process::Child,
+    client: SidechainClient,
+    main: std::process::Child,
+    zcash: std::process::Child,
+    ethereum: std::process::Child,
 }
 
-fn spawn_main(datadir: &Path, config: &Config) -> Result<tokio::process::Child> {
+impl Daemons {
+    pub fn start(url: &str, datadir: &Path, config: &Config) -> Result<Daemons> {
+        let mut first_launch = false;
+        if !datadir.join("bin").exists() {
+            download_binaries(&datadir, url)?;
+            if config.switchboard.regtest {
+                ethereum_regtest_setup(&datadir)?;
+            }
+            first_launch = true;
+        }
+        let home_dir = dirs::home_dir().unwrap();
+        if !home_dir.join(".zcash-params").exists() {
+            zcash_fetch_params(&datadir)?;
+        }
+        let daemons = Self::spawn(datadir, config)?;
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        if config.switchboard.regtest && first_launch {
+            daemons.client.activate_sidechains()?;
+        }
+        Ok(daemons)
+    }
+
+    pub fn stop(&mut self) -> Result<()> {
+        self.client.stop()?;
+        self.zcash.wait()?;
+        self.main.wait()?;
+        self.ethereum.wait()?;
+        Ok(())
+    }
+
+    fn spawn(datadir: &Path, config: &Config) -> Result<Daemons> {
+        std::fs::create_dir_all(datadir)?;
+        let main = spawn_main(datadir, config);
+        let zcash = spawn_zcash(datadir, config);
+        // FIXME: This is a temporary hack to ensure geth launches after mainchain.
+        // If mainchain isn't running when geth is launched, geth crashes.
+        std::thread::sleep(std::time::Duration::from_secs(3));
+        let ethereum = spawn_ethereum(datadir, config);
+        let client = SidechainClient::new(config)?;
+        if [&main, &zcash, &ethereum].iter().any(|r| r.is_err()) {
+            client.stop()?;
+            let ethereum_pid = ethereum.as_ref().unwrap().id();
+            std::process::Command::new("kill")
+                .args(["-s", "HUP", &ethereum_pid.to_string()])
+                .spawn()?
+                .wait()?;
+        }
+        let main = main?;
+        let zcash = zcash?;
+        let ethereum = ethereum?;
+        Ok(Daemons {
+            client,
+            main,
+            zcash,
+            ethereum,
+        })
+    }
+}
+
+fn spawn_main(datadir: &Path, config: &Config) -> Result<std::process::Child> {
     let main_datadir = datadir.join("data/main");
     std::fs::create_dir_all(&main_datadir)?;
     let default_bin = &datadir.join("bin/drivechaind");
     let bin = config.main.bin.as_ref().unwrap_or(default_bin);
-    let main = tokio::process::Command::new(bin)
+    let main = std::process::Command::new(bin)
         .arg(format!("-datadir={}", main_datadir.display()))
         .arg(format!("-rpcport={}", config.main.port))
         .arg(format!("-rpcuser={}", config.switchboard.rpcuser))
@@ -33,7 +93,7 @@ fn spawn_main(datadir: &Path, config: &Config) -> Result<tokio::process::Child> 
     Ok(main)
 }
 
-fn spawn_zcash(datadir: &Path, config: &Config) -> Result<tokio::process::Child> {
+fn spawn_zcash(datadir: &Path, config: &Config) -> Result<std::process::Child> {
     let zcash_datadir = datadir.join("data/zcash");
     std::fs::create_dir_all(&zcash_datadir)?;
     let zcash_conf_path = zcash_datadir.join("zcash.conf");
@@ -43,7 +103,7 @@ printtoconsole=1";
     std::fs::write(zcash_conf_path, zcash_conf)?;
     let default_bin = &datadir.join("bin/zcashd");
     let bin = config.zcash.bin.as_ref().unwrap_or(default_bin);
-    let zcash = tokio::process::Command::new(bin)
+    let zcash = std::process::Command::new(bin)
         .arg(format!("-datadir={}", zcash_datadir.display()))
         .arg(format!("-mainport={}", config.main.port))
         .arg(format!("-rpcport={}", config.zcash.port))
@@ -60,13 +120,12 @@ printtoconsole=1";
     Ok(zcash)
 }
 
-fn spawn_ethereum(datadir: &Path, config: &Config) -> Result<tokio::process::Child> {
+fn spawn_ethereum(datadir: &Path, config: &Config) -> Result<std::process::Child> {
     let ethereum_datadir = datadir.join("data/ethereum");
     std::fs::create_dir_all(&ethereum_datadir)?;
-    let ethereum_conf_path = ethereum_datadir.join("ethereum.conf");
     let default_bin = &datadir.join("bin/geth");
     let bin = config.ethereum.bin.as_ref().unwrap_or(default_bin);
-    let ethereum = tokio::process::Command::new(bin)
+    let ethereum = std::process::Command::new(bin)
         .arg(format!("--datadir={}", ethereum_datadir.display()))
         .arg(format!("--http.port={}", config.ethereum.port))
         .arg(format!("--main.port={}", config.main.port))
@@ -79,59 +138,32 @@ fn spawn_ethereum(datadir: &Path, config: &Config) -> Result<tokio::process::Chi
     Ok(ethereum)
 }
 
-pub async fn spawn_daemons(datadir: &Path, config: &Config) -> Result<Daemons> {
-    std::fs::create_dir_all(datadir)?;
-    let main = spawn_main(datadir, config);
-    let zcash = spawn_zcash(datadir, config);
-    // FIXME: This is a temporary hack to ensure geth launches after mainchain.
-    // If mainchain isn't running when geth is launched, geth crashes.
-    std::thread::sleep(std::time::Duration::from_secs(3));
-    let ethereum = spawn_ethereum(datadir, config);
-    if [&main, &zcash, &ethereum].iter().any(|r| r.is_err()) {
-        let client = SidechainClient::new(config)?;
-        client.stop().await?;
-        let ethereum_pid = ethereum.as_ref().unwrap().id().unwrap();
-        tokio::process::Command::new("kill")
-            .args(["-s", "HUP", &ethereum_pid.to_string()])
-            .spawn()?
-            .wait()
-            .await?;
-    }
-    let main = main?;
-    let zcash = zcash?;
-    let ethereum = ethereum?;
-    Ok(Daemons {
-        main,
-        zcash,
-        ethereum,
-    })
-}
-
-pub async fn download_binaries(datadir: &Path, url: &str) -> Result<()> {
+pub fn download_binaries(datadir: &Path, url: &str) -> Result<()> {
     const SHA256_DIGEST: &str = "adeca73e0b5e08e74b4ef20c057319bcc85fab8453deee677b74c060d3e89e29";
-    download(url, datadir, SHA256_DIGEST).await?;
+    download(url, datadir, SHA256_DIGEST)?;
     Ok(())
 }
 
-pub async fn download(url: &str, path: &Path, digest: &str) -> Result<()> {
-    let response = reqwest::get(url).await?;
-    let content = response.bytes().await?;
-    assert_eq!(sha256::digest(content.as_ref()), digest);
+pub fn download(url: &str, path: &Path, digest: &str) -> Result<()> {
+    let resp = ureq::get(url).call()?;
+    let len: usize = resp.header("Content-Length").unwrap().parse()?;
+    let mut content: Vec<u8> = Vec::with_capacity(len);
+    resp.into_reader().read_to_end(&mut content)?;
+    assert_eq!(sha256::digest(content.as_slice()), digest);
     let tar = GzDecoder::new(content.reader());
     let mut archive = Archive::new(tar);
     archive.unpack(path)?;
     Ok(())
 }
 
-pub async fn zcash_fetch_params(datadir: &Path) -> Result<()> {
-    let fetch_params = tokio::process::Command::new(datadir.join("bin/fetch-params.sh"))
+pub fn zcash_fetch_params(datadir: &Path) -> Result<()> {
+    std::process::Command::new(datadir.join("bin/fetch-params.sh"))
         .spawn()?
-        .wait()
-        .await?;
+        .wait()?;
     Ok(())
 }
 
-pub async fn ethereum_regtest_setup(datadir: &Path) -> Result<()> {
+pub fn ethereum_regtest_setup(datadir: &Path) -> Result<()> {
     const GENESIS_JSON: &str = r#"
 {
 "config": {
@@ -158,8 +190,8 @@ pub async fn ethereum_regtest_setup(datadir: &Path) -> Result<()> {
     let ethereum_datadir = datadir.join("data/ethereum");
     std::fs::create_dir_all(&ethereum_datadir)?;
     let genesis_json_path = ethereum_datadir.join("genesis.json");
-    std::fs::write(&genesis_json_path, GENESIS_JSON);
-    let ethereum = tokio::process::Command::new(datadir.join("bin/geth"))
+    std::fs::write(&genesis_json_path, GENESIS_JSON)?;
+    std::process::Command::new(datadir.join("bin/geth"))
         .arg(format!("--datadir={}", ethereum_datadir.display()))
         .arg("init")
         .arg(format!("{}", genesis_json_path.display()))
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 pub mod config;
-pub mod api;
 pub mod launcher;
-pub mod server;
-mod amount;
-mod main_client;
-mod zcash_client;
-mod ethereum_client;
+mod client;
+
+pub fn format_deposit_address(sidechain_number: usize, address: String) -> String {
+    let deposit_address: String = format!("s{}_{}_", sidechain_number, address);
+    let hash = sha256::digest(deposit_address.as_bytes()).to_string();
+    let hash: String = hash[..6].into();
+    format!("{}{}", deposit_address, hash)
+}
